4ce528d8a018b9eee9ea235e2bbee7441adf525b
Rhizamax <arnaud.lebert@orange.fr>
2026-07-15 16:27:49 +0200

    Remove trailing slash from base URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: ' https://rhizamax.github.io',
-  base: '/cabinet-dentaire-lebert/',
+  base: '/cabinet-dentaire-lebert',
 });
